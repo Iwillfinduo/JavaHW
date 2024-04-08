5b5aaf60b5e420f346114f6e1619dc8ab4a0f42a
Rostix <puten2050@yandex.ru>
2024-04-08 14:09:06 +0500

    first ver of hw2

---
+++ HW2/src/main/java/edu/phystech/hw2/contact/Contact.java
@@ -0,0 +1,47 @@
+package edu.phystech.hw2.contact;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+
+
+record Contact (String username, String email) implements Comparable<Contact> {
+    public static final String UNKNOWN_EMAIL = "unknown";
+
+    Contact {
+        if (!isValidName(username)) {
+            throw new InvalidContactFieldException("username");
+        }
+        if (!isValidEmailAddress(email)) {
+            throw new InvalidContactFieldException("email");
+        }
+
+    }
+
+    static private boolean isValidName(String username) {
+        return !username.replaceAll("\\s+", "").isEmpty();
+    }
+
+    static private boolean isValidEmailAddress(String email) {
+        if (email.equals(UNKNOWN_EMAIL)){
+            return true;
+        }
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@gmail.com";
+        Pattern p = Pattern.compile(ePattern);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
+    Contact(String username) { this(username, UNKNOWN_EMAIL); }
+
+
+    @Override
+    public int compareTo(Contact o) {
+        int cmp = Integer.compare(this.username.length(), o.username.length());
+
+        if(cmp == 0) {
+            cmp = Integer.compare(this.email.length(), o.email.length());
+        }
+        return cmp;
+    }
+}
